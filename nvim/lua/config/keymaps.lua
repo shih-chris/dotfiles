2d1c4cd21921bcad2c47abe83029e94df3ac4e49
Chris Shih <chs820@gmail.com>
2023-05-05 16:46:23 -0400

    update keybinds

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,22 @@
--- Insert --
+-----------------
+-- Insert Mode --
+-----------------
+
 -- Press jk fast to exit insert mode 
 vim.keymap.set("i", "jk", "<ESC>", { desc = 'Exit insert mode' })
 
--- Normal --
+
+-----------------
+-- Normal Mode --
+-----------------
+
+-- Unbind space
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 -- Better window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = 'Window Navigation - Left' })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = 'Window Navigation - Down' })
@@ -23,7 +37,11 @@ vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { desc = 'Buffer Navigation - Pre
 vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = 'Move text down - Normal Mode' })
 vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = 'Move text up - Normal Mode' })
 
--- Visual --
+
+-----------------
+-- Visual Mode --
+-----------------
+
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv", { desc = 'Indent Line' })
 vim.keymap.set("v", ">", ">gv", { desc = 'De-indent Line' })
@@ -33,7 +51,11 @@ vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", { desc = 'Move text down - Visual m
 vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", { desc = 'Move text up - Visual mode' })
 vim.keymap.set("v", "p", '"_dP', { desc = 'Paste and retain value - Visual mode'})
 
--- Visual Block --
+
+-----------------------
+-- Visual Block Mode --
+-----------------------
+
 -- Move text up and down
 vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", { desc = 'Move text down - Visual Block mode' })
 vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", { desc = 'Move text up - Visual Block mode' })
