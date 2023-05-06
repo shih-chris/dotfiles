295cb508aabd78023e8edcfdf64825eeca69f530
Chris Shih <chs820@gmail.com>
2023-05-05 20:55:08 -0400

    Update keymaps for neo-tree

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -18,38 +18,40 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = 'Window Navigation - Left' })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = 'Window Navigation - Down' })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = 'Window Navigation - Up' })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = 'Window Navigation - Right' })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Window Navigation - Left' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Window Navigation - Down' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Window Navigation - Up' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Window Navigation - Right' })
 
 -- Resize with arrows
-vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", { desc = 'Resize Window - Up' })
-vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", { desc = 'Resize Window - Down' })
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = 'Resize Window - Left' })
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = 'Resize Window - Right' })
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', { desc = 'Resize Window - Up' })
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', { desc = 'Resize Window - Down' })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Resize Window - Left' })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Resize Window - Right' })
 
 -- Navigate buffers
-vim.keymap.set("n", "<S-l>", ":bnext<CR>", { desc = 'Buffer Navigation - Next' })
-vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { desc = 'Buffer Navigation - Previous' })
+vim.keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Buffer Navigation - Next' })
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { desc = 'Buffer Navigation - Previous' })
 
 -- Move text up and down
-vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = 'Move text down - Normal Mode' })
-vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = 'Move text up - Normal Mode' })
+vim.keymap.set('n', '<A-j>', '<Esc>:m .+1<CR>==gi', { desc = 'Move text down - Normal Mode' })
+vim.keymap.set('n', '<A-k>', '<Esc>:m .-2<CR>==gi', { desc = 'Move text up - Normal Mode' })
 
+-- Open Neotree
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree action=focus source=filesystem position=left toggle=true<CR>', { desc = 'Toggle File [e]xplorer' }) 
 
 -----------------
 -- Visual Mode --
 -----------------
 
 -- Stay in indent mode
-vim.keymap.set("v", "<", "<gv", { desc = 'Indent Line' })
-vim.keymap.set("v", ">", ">gv", { desc = 'De-indent Line' })
+vim.keymap.set('v', '<', '<gv', { desc = 'Indent Line' })
+vim.keymap.set('v', '>', '>gv', { desc = 'De-indent Line' })
 
 -- Move text up and down
-vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", { desc = 'Move text down - Visual mode' })
-vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", { desc = 'Move text up - Visual mode' })
-vim.keymap.set("v", "p", '"_dP', { desc = 'Paste and retain value - Visual mode'})
+vim.keymap.set('v', '<A-j>', ':m .+1<CR>==', { desc = 'Move text down - Visual mode' })
+vim.keymap.set('v', '<A-k>', ':m .-2<CR>==', { desc = 'Move text up - Visual mode' })
+vim.keymap.set('v', 'p', '"_dP', { desc = 'Paste and retain value - Visual mode'})
 
 
 -----------------------
@@ -57,7 +59,7 @@ vim.keymap.set("v", "p", '"_dP', { desc = 'Paste and retain value - Visual mode'
 -----------------------
 
 -- Move text up and down
-vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", { desc = 'Move text down - Visual Block mode' })
-vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", { desc = 'Move text up - Visual Block mode' })
-vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", { desc = 'Move text down - Visual Block mode' })
-vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", { desc = 'Move text up - Visual Block mode' })
+vim.keymap.set('x', 'J', ":move '>+1<CR>gv-gv", { desc = 'Move text down - Visual Block mode' })
+vim.keymap.set('x', 'K', ":move '<-2<CR>gv-gv", { desc = 'Move text up - Visual Block mode' })
+vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv-gv", { desc = 'Move text down - Visual Block mode' })
+vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv-gv", { desc = 'Move text up - Visual Block mode' })
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -5,5 +5,19 @@ return {
     {
         'nvim-neo-tree/neo-tree.nvim',
         branch = 'v2.x',
+        config = function()
+            vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+            require('neo-tree').setup({
+                window = {
+                    width = 30,
+                    mappings = {
+                        ['<cr>'] = 'focus_preview',
+                        ['l'] = 'open',
+                        ['h'] = 'close_node',
+                    }
+                },
+            })
+        end
     },
 }
