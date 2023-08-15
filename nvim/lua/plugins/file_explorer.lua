1e93d475b23982362626939678b342441155fc0a
Chris Shih <chs820@gmail.com>
2023-08-15 08:46:39 -0400

    update indentation highlighting behavior

--- nvim/lua/plugins/file_explorer.lua
+++ nvim/lua/plugins/file_explorer.lua
@@ -2,11 +2,11 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      close_if_last_window = true,
-      popup_border_style = "rounded",
+      -- close_if_last_window = true,
+      -- popup_border_style = "rounded",
       window = {
-        position = "float",
-        width = "100%",
+        -- position = "float",
+        -- width = "100%",
         mappings = {
           -- ['<cr>'] = 'focus_preview',
           ["l"] = "open",
---
+++ nvim/lua/plugins/indentation.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "echasnovski/mini.indentscope",
+    enabled = false,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = function(_, opts)
+      opts.show_current_context = true
+    end,
+  },
+}
