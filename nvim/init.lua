4599b2770b8508c6c15ecf734b55221e737035de
Chris Shih <chs820@gmail.com>
2023-08-16 22:44:13 -0400

    attempts to update treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- manually add bigquery sql parser
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_config.sql_bigquery = {
+--   install_info = {
+--     url = "~/projects/tree-sitter-sql-bigquery", -- local path or git repo
+--     files = { "src/parser.c", "src/scanner.cc" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+--     -- optional entries:
+--     branch = "main", -- default branch in case of git repo if different from master
+--     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--   },
+--   filetype = "sql", -- if filetype does not match the parser name
+-- }
+--
+-- vim.treesitter.language.register("sql_biquery", "sql") -- the sql filetype will use the sql_bigquery parser and queries.
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,42 @@
+return {
+  { "nvim-treesitter/playground" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      playground = { enable = true },
+      ensure_installed = {
+        "bash",
+        "c",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "sql_bigquery",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    },
+  },
+}
