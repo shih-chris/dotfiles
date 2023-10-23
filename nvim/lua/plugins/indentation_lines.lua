938bad8fcbc3efb73242f60d8593c2911d3ac3bd
Chris Shih <chs820@gmail.com>
2023-10-23 19:10:17 -0400

    fix indentation_line error

--- nvim/lua/plugins/indentation_lines.lua
+++ nvim/lua/plugins/indentation_lines.lua
@@ -3,10 +3,10 @@ return {
     "echasnovski/mini.indentscope",
     enabled = false,
   },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = function(_, opts)
-      opts.show_current_context = true
-    end,
-  },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   opts = function(_, opts)
+  --     opts.show_current_context = true
+  --   end,
+  -- },
 }
