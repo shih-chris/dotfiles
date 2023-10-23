b6090aac6ecb31c9156f1bf2e877f47c7e1ffdbf
Chris Shih <chs820@gmail.com>
2023-10-23 19:57:54 -0400

    add project switching

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "telescope.nvim",
+    dependencies = {
+      -- project management
+      {
+        "ahmedkhalf/project.nvim",
+        opts = {
+          manual_mode = true,
+        },
+        event = "VeryLazy",
+        config = function(_, opts)
+          require("project_nvim").setup(opts)
+          require("lazyvim.util").on_load("telescope.nvim", function()
+            require("telescope").load_extension("projects")
+          end)
+        end,
+        keys = {
+          { "<leader>pl", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+        },
+      },
+    },
+  },
+}
