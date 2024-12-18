ee3299405264d7055f1705a68211d157eaedd425
Chris Shih <chs820@gmail.com>
2024-12-17 22:44:55 -0500

    move to ui.lua

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      indent = {
+        animate = {
+          enabled = false,
+        },
+      },
+      notifier = { enabled = false },
+      scroll = { enabled = false },
+    },
+  },
+}
