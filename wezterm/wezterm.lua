48775e63fa336f0068ce15010982ebe35b17c813
Chris Shih <chs820@gmail.com>
2024-12-17 23:37:53 -0500

    update wezterm quality of life

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,12 +5,13 @@ local config = wezterm.config_builder()
 -- Fonts
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 17
-config.line_height = 1.0
+config.line_height = 1.05
 
 -- UI
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.max_fps = 120
+config.animation_fps = 120
 
 -- Colors
 
@@ -25,7 +26,7 @@ config.max_fps = 120
 -- config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 config.color_scheme = "Catppuccin Macchiato"
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 40
 
 -- Mouse
