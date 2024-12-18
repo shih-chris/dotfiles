2ce8ce3a29cba2b9a2801d8710ce196152016dcb
Chris Shih <chs820@gmail.com>
2024-12-17 23:12:42 -0500

    update bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,51 +1,25 @@
 return {
   {
     "akinsho/bufferline.nvim",
+    event = "VeryLazy",
     keys = {
-      -- update descriptions
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "toggle [p]in" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "delete non-[P]inned buffers" },
-
-      -- remap buffer close all others
-      { "<leader>bo", false },
-      { "<leader>bD", "<Cmd>BufferLineCloseOthers<CR>", desc = "[D]elete all other buffers" },
-
-      -- remove unnecessary buffer keymap
+      { "<leader>bp", false },
+      { "<leader>bP", false },
       { "<leader>br", false },
       { "<leader>bl", false },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
       { "[b", false },
       { "]b", false },
+      { "[B", false },
+      { "]B", false },
     },
-  },
-  {
-    "echasnovski/mini.bufremove",
-    keys = {
-      {
-        "<leader>bd",
-        function()
-          local bd = require("mini.bufremove").delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-            if choice == 1 then -- Yes
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then -- No
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-        desc = "[d]elete buffer",
-      },
-    -- stylua: ignore
-      { "<leader>bD", "<Cmd>BufferLineCloseOthers<CR>", desc = "[D]elete all other buffers" },
-      {
-        "<leader>bF",
-        function()
-          require("mini.bufremove").delete(0, true)
-        end,
-        desc = "delete buffer ([F]orce)",
+    opts = {
+      options = {
+        show_buffer_icons = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = "thin",
       },
     },
   },
