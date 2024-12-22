d65339e6f2108bac843059aa5f9045ad27234a4d
Chris Shih <chs820@gmail.com>
2024-12-21 20:44:27 -0500

    create start of new nvim from scratch

---
+++ nvim_cshih/README.md
@@ -0,0 +1,3 @@
+# NVIM config from scratch
+
+Largely based on the structure of [kickstart-modular.nvim](https://github.com/dam9000/kickstart-modular.nvim/tree/master)
---
+++ nvim_cshih/init.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+require("options")
+require("keymaps")
+require("autocmds")
+require("lazy-bootstrap")
+require("plugins")
