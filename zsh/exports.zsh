9eb4285018af8641cdd01512dd07e649aafd65e9
Chris Shih <chs820@gmail.com>
2024-12-26 14:26:37 -0500

    update more fzf color defaults

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -21,9 +21,9 @@ pyenv() {
 
 # fzf exports
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=bg+:#363a4f,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
---color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796,gutter:-1 \
 --color=selected-bg:#494d64 \
 --multi"
 
