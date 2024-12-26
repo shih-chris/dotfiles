569a3f357dcaa017c56388c3ec5a31d96609c755
Chris Shih <chs820@gmail.com>
2024-12-26 14:06:00 -0500

    add mini.icons

--- nvim_cshih/lua/plugins/search.lua
+++ nvim_cshih/lua/plugins/search.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"ibhagwan/fzf-lua",
-		-- dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = { "echasnovski/mini.icons" },
 		config = function()
 			local fzf_lua = require("fzf-lua")
 
