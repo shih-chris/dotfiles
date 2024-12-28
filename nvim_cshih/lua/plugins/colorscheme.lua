b141769fdc72576d6147d5179fa029c0c2ec6b64
Chris Shih <chs820@gmail.com>
2024-12-28 12:35:50 -0500

    add neo-tree instead of mini-files

--- nvim_cshih/lua/plugins/colorscheme.lua
+++ nvim_cshih/lua/plugins/colorscheme.lua
@@ -27,21 +27,43 @@ return {
       end,
       default_integration = false,
       integrations = {
+        cmp = true,
         fzf = true,
+        neotree = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
       },
     },
     -- specs = {
-    --   {
-    --     "akinsho/bufferline.nvim",
-    --     optional = true,
-    --     opts = function(_, opts)
-    --       if (vim.g.colors_name or ""):find("catppuccin") then
-    --         opts.highlights = require("catppuccin.groups.integrations.bufferline").get({
-    --           styles = { "bold" },
-    --         })
-    --       end
-    --     end,
-    --   },
-    -- },
-  },
-}
+      --   {
+        --     "akinsho/bufferline.nvim",
+        --     optional = true,
+        --     opts = function(_, opts)
+          --       if (vim.g.colors_name or ""):find("catppuccin") then
+          --         opts.highlights = require("catppuccin.groups.integrations.bufferline").get({
+            --           styles = { "bold" },
+            --         })
+            --       end
+            --     end,
+            --   },
+            -- },
+          },
+        }
---
+++ nvim_cshih/lua/plugins/neo-tree.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "open file tre[e]",
+        remap = true,
+      },
+    },
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+          ["<space>"] = "none",
+          ["Y"] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "Cop[Y] Path to Clipboard",
+          },
+        },
+      },
+    },
+  },
+}
