b4f291cc0abeff893dae0fbb13cec534d7260688
Chris Shih <chs820@gmail.com>
2024-12-30 08:52:10 -0500

    remove lazy loading for lsp/treesitter

--- nvim_cshih/lua/plugins/lspconfig.lua
+++ nvim_cshih/lua/plugins/lspconfig.lua
@@ -16,7 +16,6 @@ return {
   {
     -- Main LSP Configuration
     "neovim/nvim-lspconfig",
-    lazy = true,
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
