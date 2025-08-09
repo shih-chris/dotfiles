e0b9a4bc005e3f0b69f902f6f4f3622fd1d37546
Chris Shih <chs820@gmail.com>
2025-08-09 10:08:35 -0400

    add markdown support with markdown-preview

---
+++ nvim_cshih/lua/plugins/markdown.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    keys = {
+      {
+        "<leader>cr",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+}
