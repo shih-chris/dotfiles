0436dfef543ef3650fc7b26d49abe72885a68a46
Chris Shih <chs820@gmail.com>
2025-11-03 15:08:55 -0500

    make fzf updates

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -22,9 +22,10 @@ pyenv() {
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#363a4f,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#c6a0f6 \
---color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796,gutter:-1 \
+--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
 --color=selected-bg:#494d64 \
---multi"
+--multi \
+--gutter ' '"
 
 # Other exports
 export LANG=en_US.UTF-8
